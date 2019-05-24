ea0b266e45f983b66debd661960a73adf6ad1484
skuznet <azon.sk@gmail.com>
2019-05-24 20:30:22 +0300

    add 9 lesson

---
+++ src/main/java/lesson9/Main.java
@@ -0,0 +1,82 @@
+package lesson9;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+//        Character[] characters = new Character[10];
+//
+//        characters[0] = 'B';
+//        characters[1] = 'a';
+//        characters[2] = 'r';
+//        characters[3] = 's';
+//        characters[4] = 'i';
+//        characters[5] = 'k';
+//
+//        Map<Character[], String> map = new HashMap<>();
+//
+//        map.put(characters, "Barsik");
+//
+//        map.forEach((s, v) -> {
+//            for (Character character : s) {
+//                if (character != null) {
+//                    System.out.print(character);
+//                }
+//            }
+//            System.out.print(" ");
+//            System.out.println(v);
+//        });
+//
+//        characters[0] = 'M';
+//        characters[1] = 'u';
+//        characters[2] = 'r';
+//        characters[3] = 'z';
+//        characters[4] = 'i';
+//        characters[5] = 'k';
+//
+//        map.put(characters, "Murzik");
+//        map.forEach((s, v) -> {
+//            for (Character character : s) {
+//                if (character != null) {
+//                    System.out.print(character);
+//                }
+//            }
+//            System.out.print(" ");
+//            System.out.println(v);
+//        });
+//        Object[] array = new Object[16];
+//        List<String> list = new LinkedList<>();
+//        list.add("barsik");
+//        list.add("murzik");
+//        array[1] = list;
+
+
+//
+//        System.out.println(array[1]);
+//        Set<Cat> set = new HashSet<>();
+//
+        Cat barsik = new Cat(1, "Barsik", "good");
+        System.out.println(barsik);
+//
+//        System.out.println(barsik.hashCode());
+//        set.add(barsik);
+//
+//        System.out.println(set.contains(barsik));
+//        barsik.setDescription("angry");
+//
+//        System.out.println(barsik.hashCode());
+//        System.out.println(set.contains(barsik));
+//
+//        set.add(barsik);
+//        System.out.println(set.contains(barsik));
+//
+//        System.out.println(set);
+//
+//        Set<Cat> newSet = new HashSet<>(set);
+//        System.out.println(newSet);
+
+    }
+}
