b32013da45d3df3812c2ba2cf8284d2039eca82c
Vlad Nesterov <vladislavnesterov_1@icloud.com>
2019-06-06 01:15:18 +0300

    ...

--- src/main/java/task06/Interf.java
+++ src/main/java/task06/Interf.java
@@ -13,12 +13,12 @@ public class Interf {
         float firstNum = in.nextFloat();
         float secondNum = in.nextFloat();
         Map<String, MatchOperate> opa = new HashMap<>();
-        opa.put("Sum= ", (a, b) -> a + b);
-        opa.put("Div= ", (a, b) -> a / b);
-        opa.put("Multiplication= ", (a, b) -> a * b);
-        opa.put("Minus= ", (a, b) -> a - b);
+        opa.put("Sum", (a, b) -> a + b);
+        opa.put("Div", (a, b) -> a / b);
+        opa.put("Multiplication", (a, b) -> a * b);
+        opa.put("Minus", (a, b) -> a - b);
         for (Map.Entry<String, MatchOperate> entry : opa.entrySet()) {
-            System.out.println(entry.getKey() + entry.getValue().apply(firstNum, secondNum));
+            System.out.println(entry.getKey() +"= "+ entry.getValue().apply(firstNum, secondNum));
         }
     }
 }
