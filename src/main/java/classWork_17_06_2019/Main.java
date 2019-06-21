d6521d48a50d64b7c39603a6518d28a77cbc1d47
Vlad Nesterov <vladislavnesterov_1@icloud.com>
2019-06-21 15:16:14 +0300

    Rewrite Class Names, don't use random. Optimize Class Main

--- src/main/java/classWork_17_06_2019/Main.java
+++ src/main/java/classWork_17_06_2019/Main.java
@@ -1,6 +1,5 @@
 package classWork_17_06_2019;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,18 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Map<String, Object> bestScore_and_ArrayScore;
         bestScore_and_ArrayScore = Score.getSoreMaxValue();
-        List<String> lll;
-        lll= Names.getNames();
-//        for (Map.Entry<String, Object> entry : bestScore_and_ArrayScore.entrySet()) {
-//            System.out.println(entry.getKey() + "= " + entry.getValue());
-//        }
-////        String resultStr = str.substring(str.indexOf('.') + 1, str.indexOf(':'));
-//        String secondeName=(lll.get(((int) bestScore_and_ArrayScore.get("Number winner"))-1)).toString();
-//        System.out.println("Name winner=" + secondeName.substring(secondeName.length() - 6, secondeName.length()));
-        System.out.println("Number winner="+bestScore_and_ArrayScore.get("Number winner"));
-        System.out.println("Max points="+bestScore_and_ArrayScore.get("Max points"));
+        List<String> listNameGetName;
+        listNameGetName = Names.getNames();
+        System.out.println("Number winner=" + bestScore_and_ArrayScore.get("Number winner"));
+        System.out.println("Max points=" + bestScore_and_ArrayScore.get("Max points"));
 
-        System.out.println("Name winner=" + (lll.get(((int) bestScore_and_ArrayScore.get("Number winner"))-1)));
+        System.out.println("Name winner=" + (listNameGetName.get(((int) bestScore_and_ArrayScore.get("Number winner")) - 1)));
         int[][] arrayPoint = (int[][]) Score.getSoreMaxValue().get("Get all points");
         for (int l = 0; l < 20; l++) {
             System.out.print(Names.getNames().get(l) + "  ");
--- src/main/java/classWork_17_06_2019/Score.java
+++ src/main/java/classWork_17_06_2019/Score.java
@@ -12,17 +12,17 @@ class Score {
         for (int i = 0; i < 20; i++) {
             sumPoints = 0;
             for (int j = 0; j < 5; j++) {
-                arrayScore[i][j] = 10 + (int) (Math.random() * 90);
+                arrayScore[i][j] = 1 + (int) (Math.random() * 90);
                 sumPoints = sumPoints + arrayScore[i][j];
             }
             bestScore.add(sumPoints);
         }
         Map<String, Object> map = new HashMap<>();
         Integer maxPoints = max(bestScore);
-        Integer numberWinner = bestScore.indexOf(maxPoints)+1;
+        Integer numberWinner = bestScore.indexOf(maxPoints) + 1;
 
         map.put("Max points", maxPoints);
-        map.put("Number winner",numberWinner);
+        map.put("Number winner", numberWinner);
         map.put("Get all points", arrayScore);
         map.put("Sum for 5 test", bestScore);
 
